68476198258341f4faae38de661f0b767efb2f5b
Md Azizul <khanazizul1199@gmail.com>
2024-08-17 17:38:30 +0600

    implement responsive design

--- src/common/Navbar.jsx
+++ src/common/Navbar.jsx
@@ -3,17 +3,18 @@
 import { Link } from "react-router-dom";
 import Login from "./Login";
 import useAuth from "../hooks/useAuth";
+import { useState } from "react";
 const Navbar = () => {
 	const { user } = useAuth();
-	console.log(user);
+	const [showMenu, setShowMenu] = useState(false);
 	const navLinks = (
 		<>
-			<li className="p-2 border-e-0 hover:border-rose-500 hover:text-rose-400 transition-all duration-300">
+			<li className="p-2 border-e-0 hover:border-rose-500 lg:hover:text-rose-400 transition-all duration-300">
 				<Link to="/" className="underline-offset-4 underline">
 					Home
 				</Link>
 			</li>
-			<li className="p-2 border-l-2 hover:border-rose-500 hover:text-rose-400 transition-all duration-300">
+			<li className="p-2 border-l-2 hover:border-rose-500 lg:hover:text-rose-400 transition-all duration-300">
 				<div className="dropdown dropdown-hover">
 					<span
 						tabIndex={0}
@@ -35,12 +36,12 @@ const Navbar = () => {
 					</ul>
 				</div>
 			</li>
-			<li className="p-2 border-l-2 hover:border-rose-500 hover:text-rose-400 transition-all duration-300">
+			<li className="p-2 border-l-2 hover:border-rose-500 lg:hover:text-rose-400 transition-all duration-300">
 				<Link to="/" className="underline-offset-4 underline">
 					Supports
 				</Link>
 			</li>
-			<li className="p-2 border-l-2 hover:border-rose-500 hover:text-rose-400 transition-all duration-300">
+			<li className="p-2 border-l-2 hover:border-rose-500 lg:hover:text-rose-400 transition-all duration-300">
 				<Link to="/" className="underline-offset-4 underline">
 					Reviews
 				</Link>
@@ -56,13 +57,52 @@ const Navbar = () => {
 				Shop
 			</h1>
 			<section className="flex items-center justify-evenly max-w-4xl mx-auto">
+				<nav className="lg:hidden block">
+					{showMenu ? (
+						<button
+							onClick={() => setShowMenu(!showMenu)}
+							className="btn btn-circle"
+						>
+							<svg
+								className="swap-on fill-current"
+								xmlns="http://www.w3.org/2000/svg"
+								width="32"
+								height="32"
+								viewBox="0 0 512 512"
+							>
+								<polygon points="400 145.49 366.51 112 256 222.51 145.49 112 112 145.49 222.51 256 112 366.51 145.49 400 256 289.49 366.51 400 400 366.51 289.49 256 400 145.49" />
+							</svg>
+						</button>
+					) : (
+						<button
+							onClick={() => setShowMenu(!showMenu)}
+							className="btn btn-circle"
+						>
+							<svg
+								className="swap-off fill-current"
+								xmlns="http://www.w3.org/2000/svg"
+								width="32"
+								height="32"
+								viewBox="0 0 512 512"
+							>
+								<path d="M64,384H448V341.33H64Zm0-106.67H448V234.67H64ZM64,128v42.67H448V128Z" />
+							</svg>
+						</button>
+					)}
+				</nav>
 				<img
 					src="/logo.png"
 					className="w-20 hover:scale-150 duration-300 transition-all cursor-pointer"
 					alt="logo"
 				/>
-				<nav className="max-w-4xl mx-auto p-x-2">
-					<ul className="flex justify-center space-x-10 border-t-2 text-gray-200 font-semibold group-hover:border-gray-700">
+				<nav
+					className={`z-50 max-w-4xl mx-auto p-x-2 absolute lg:static lg:block ${
+						showMenu
+							? "-translate-x-16 top-24 p-6 lg:p-0 rounded-sm duration-500 transition-all bg-rose-400"
+							: "-translate-x-80 lg:-translate-x-0"
+					} absolute lg:static lg:block `}
+				>
+					<ul className="flex flex-col lg:flex-row justify-center space-x-10 border-t-2 text-gray-200 font-semibold group-hover:border-gray-700">
 						{navLinks}
 					</ul>
 				</nav>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -14,7 +14,7 @@ const Home = () => {
 	const [brand, setBrand] = useState("");
 	const [priceRange, setPriceRange] = useState("");
 	const [category, setCategory] = useState("");
-
+	const [showFiltering, setShowFiltering] = useState(false);
 	// shortcut for query
 	// const [queryParams, setQueryParams] = useState({
 	// 	q: 'shoes',
@@ -82,6 +82,7 @@ const Home = () => {
 	// if (!products.length && !search) return <h1 className="text-9xl">Loading</h1>;
 	return (
 		<>
+			{/* search bar */}
 			<div className="flex flex-col md:flex-row gap-3 max-w-5xl mx-auto">
 				<form onSubmit={searchSubmit} className="flex md:w-3/4">
 					<input
@@ -97,43 +98,59 @@ const Home = () => {
 						Search
 					</button>
 				</form>
-				<select
-					onChange={(e) => {
-						setSort(e.target.value);
-					}}
-					className="h-10 max-w-fit border-2 border-sky-500 focus:outline-none focus:border-sky-500 text-sky-500 rounded px-2 md:px-3 py-0 md:py-1 tracking-wider"
-				>
-					<option defaultValue="sorting" selected disabled hidden>
-						Sorting
-					</option>
-					<option value="default">Default</option>
-					<option disabled>Price</option>
-					<option className="ml-10" value="lowToHigh">
-						Low To High
-					</option>
-					<option value="highToLow" className="ml-3">
-						High To Low
-					</option>
-					<option disabled>Date Added</option>
-					<option value="newest">Newest</option>
-					<option value="oldest">Oldest</option>
-				</select>
+				<div className="flex justify-between mx-8">
+					<select
+						onChange={(e) => {
+							setSort(e.target.value);
+						}}
+						className="h-10 max-w-fit border-2 border-sky-500 focus:outline-none focus:border-sky-500 text-sky-500 rounded px-2 md:px-3 py-0 md:py-1 tracking-wider"
+					>
+						<option defaultValue="sorting" selected disabled hidden>
+							Sorting
+						</option>
+						<option value="default">Default</option>
+						<option disabled>Price</option>
+						<option className="ml-10" value="lowToHigh">
+							Low To High
+						</option>
+						<option value="highToLow" className="ml-3">
+							High To Low
+						</option>
+						<option disabled>Date Added</option>
+						<option value="newest">Newest</option>
+						<option value="oldest">Oldest</option>
+					</select>
+					<button
+						onClick={() => setShowFiltering(!showFiltering)}
+						className="btn btn-circle lg:hidden"
+					>
+						{showFiltering ? (
+							<img width="32" height="32" src="/filter.svg" alt="filter icon" />
+							) : (
+								<img width="32" height="32" src="/cross_filter.svg" alt="filter icon" />
+						)}
+					</button>
+				</div>
 			</div>
-			<div className="grid grid-cols-8 my-3">
-				<section className="col-span-6 w-full grid grid-cols-3 gap-2 p-2 lg:p-5">
+			<div className="flex gap-1 my-3">
+				<section className="mx-auto w-full lg:w-[80%] grid grid-cols-1 justify-items-center md:grid-cols-2 lg:grid-cols-3 gap-2 p-2">
 					{products.length ? (
 						products.map((product) => (
 							<ProductCard key={product._id} product={product} />
 						))
 					) : (
-							<img
-								src="/not-found.png"
-								alt="not found image"
-								className="col-span-full h-fit p-5"
-							/>
+						<img
+							src="/not-found.png"
+							alt="not found image"
+							className="col-span-full h-fit p-5"
+						/>
 					)}
 				</section>
-				<section className="col-span-2 w-full border rounded-xl p-5">
+				<section
+					className={`border  rounded-xl p-5 ${
+						showFiltering ? "hidden" : "block absolute right-4 bg-red-400"
+					} lg:block`}
+				>
 					<div className="border rounded-md p-3 w-full mx-auto max-w-2xl">
 						<h4 className="text-xl font-semibold">Brand Name</h4>
 						<label className="flex bg-gray-100 text-gray-700 rounded-md px-2 py-1 my-1  hover:bg-indigo-300 cursor-pointer ">
@@ -197,13 +214,13 @@ const Home = () => {
 						</label>
 					</div>
 					<button
-						className="btn btn-ghost btn-success border outline-dashed mx-auto mt-4"
+						className="btn btn-ghost btn-success border btn-sm outline-dashed mx-auto mt-4"
 						onClick={handleFiltering}
 					>
 						Apply
 					</button>
 					<button
-						className="btn btn-ghost btn-success border outline-dashed ml-5 mt-4"
+						className="btn btn-ghost btn-success btn-sm border outline-dashed ml-5 mt-4"
 						onClick={() => {
 							setSearch("");
 							setSort("");
